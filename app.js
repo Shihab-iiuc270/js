61f9dc14505ea5211dfceecb697e97b673d42446
Shihab <mohammadshihabuddin7@gmail.com>
2025-11-14 21:41:51 +0600

    event listener

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const allbox = document.getElementsByClassName("box"
-);
+// const allbox = document.getElementsByClassName("box"
+// );
 
-for(let i = 0; i<allbox.length;i++){
-    const element = allbox[i];
-    element.style.background ="green";
-    if(element.innerText=="box-5"){
-        element.style.background = "red"
-    }
+// for(let i = 0; i<allbox.length;i++){
+//     const element = allbox[i];
+//     element.style.background ="green";
+//     if(element.innerText=="box-5"){
+//         element.style.background = "red"
+//     }
 
 
-}
+// }
+var a = document.querySelector("h1")
+a.addEventListener("click",function(){
+    console.log("hi")
+})
