97e07b1670853f1881df0b8c886794d17666060b
Shihab <mohammadshihabuddin7@gmail.com>
2025-11-14 22:09:47 +0600

    bulb on off

--- app.js
+++ app.js
@@ -10,9 +10,27 @@
 
 
 // }
-var a = document.querySelector("h1")
-a.addEventListener("click",function(){
-    a.innerHTML ="hello"
-    a.style.color = "yellow"
-    a.style.backgroundColor ='black'
-})
+// var a = document.querySelector("h1")
+// a.addEventListener("click",function(){
+//     a.innerHTML ="hello"
+//     a.style.color = "yellow"
+//     a.style.backgroundColor ='black'
+// })
+ var bulb = document.querySelector("#bulb")
+ var btn = document.querySelector("button")
+
+ var flag = 0;
+
+ btn.addEventListener("click",function(){
+    if(!flag){
+        bulb.style.backgroundColor ="yellow"
+        console.log("click on")
+        flag = 1;
+    }
+    else{
+        bulb.style.backgroundColor ="white"
+        console.log("click of")
+        flag = 0;
+
+    }
+ })
